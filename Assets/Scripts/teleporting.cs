4d29213d5290960d37cedcbd554080d13ffecefd
Anika Hussen <ah3866@ad.nyu.edu>
2019-10-07 10:52:55 -0400

    teleporting

---
+++ Assets/Scripts/teleporting.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleporting : MonoBehaviour
+{
+    public Transform teleportTarget; //new spot
+    public GameObject thePlayer; //the object that is moving
+
+    public void move()//collision
+    {
+        thePlayer.transform.position = teleportTarget.transform.position; //translate (equate the location to new location)
+    }
+}
