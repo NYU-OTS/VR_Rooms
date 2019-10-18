d098e2e5be40577f19628340151224d11011cd54
ShuyiLU <pudding0129@hotmail.com>
2019-10-18 11:15:00 -0400

    add ui sign

---
+++ Assets/Scripts/NavigateRoom.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NavigateRoom : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject sign;
+
+    public void ShowSign()
+    {
+        sign.SetActive(true);
+    }
+
+    public void HideSign()
+    {
+        sign.SetActive(false);
+    }
+
+    void Start()
+    {
+        sign.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
